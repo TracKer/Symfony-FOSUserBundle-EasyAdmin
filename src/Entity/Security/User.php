519f708c54a30fa798abe44cc58810fd62fb24ff
Andrey Rudenko <andrey.rudenko@gmail.com>
2018-07-14 22:06:46 +0300

    Fix Group class and table name.

--- src/Entity/Security/User.php
+++ src/Entity/Security/User.php
@@ -19,9 +19,9 @@ class User extends BaseUser {
   protected $id;
 
   /**
-   * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Security\Group")
+   * @ORM\ManyToMany(targetEntity="App\Entity\Security\Group")
    * @ORM\JoinTable(
-   *   name="fos_user_user_group",
+   *   name="user_group",
    *   joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
    *   inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
    * )
