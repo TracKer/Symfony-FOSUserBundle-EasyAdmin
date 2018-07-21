7c21b911870f854bc87a00730a6ee05edca73aeb
Andrey Rudenko <andrey.rudenko@gmail.com>
2018-07-22 00:24:04 +0300

    Implement roles helper service.

---
+++ src/Services/RolesHelper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Role\Role;
+use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
+
+/**
+ * Class RolesHelper
+ * @package App\Services
+ */
+class RolesHelper {
+  /**
+   * @var TokenStorage
+   */
+  private $tokenStorage;
+
+  /**
+   * @var RoleHierarchyInterface
+   */
+  private $roleHierarchy;
+
+  /**
+   * @var string[]
+   */
+  private $roles = [];
+
+  public function __construct(TokenStorage $tokenStorage, RoleHierarchyInterface $roleHierarchy) {
+    $this->tokenStorage = $tokenStorage;
+    $this->roleHierarchy = $roleHierarchy;
+  }
+
+  public function getRoles() {
+    if ($this->roles) {
+      return $this->roles;
+    }
+
+    $user = $this->tokenStorage->getToken()->getUser();
+    $userRoles = $user->getRoles();
+
+    foreach ($userRoles as &$role) {
+      $role = new Role($role);
+    }
+
+    $roles = $this->roleHierarchy->getReachableRoles($userRoles);
+    foreach ($roles as &$role) {
+      $role = $role->getRole();
+    }
+    return $roles;
+  }
+}
