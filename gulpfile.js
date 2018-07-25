9839214b21005d340e952e5d6c1775d7e63ba56e
Andrey Rudenko <andrey.rudenko@gmail.com>
2018-07-26 00:50:22 +0300

    Make gulp to track changes in all scss files.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
-  return gulp.src('./assets/sass/app.scss')
+  return gulp.src('./assets/sass/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(rename({suffix: '.min'}))
