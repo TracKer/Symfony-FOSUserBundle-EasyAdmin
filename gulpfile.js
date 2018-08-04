02a9d742a3c0d0a95b82b486abee673f3a852239
Andrey Rudenko <andrey.rudenko@gmail.com>
2018-08-05 01:01:46 +0300

    Make gulp to support js bundles.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var del = require('del');
+var mergeStream = require('merge-stream');
+var streamQueue = require('streamqueue');
+var clearRequire = require('clear-require');
 
 gulp.task('sass', function () {
   return gulp.src('./assets/sass/*.scss')
@@ -12,17 +18,44 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('img', function() {
-  gulp.src([
-    'assets/images/**/*'
-  ])
-    .pipe(gulp.dest('public/images'));
+gulp.task('js-clear', function () {
+  del(['./public/js/*.*']);
 });
 
-gulp.task('watch', ['img', 'sass'], function () {
+gulp.task('js', ['js-clear'], function () {
+  clearRequire('./gulp-bundles.js');
+  var bundles = require('./gulp-bundles.js').bundles;
+
+  return mergeStream(
+    bundles.map(
+      function (bundle) {
+        return streamQueue({objectMode: true},
+          gulp.src(bundle.dependencies).pipe(sourcemaps.init()),
+          gulp.src(bundle.sources).pipe(sourcemaps.init()).pipe(uglify())
+        )
+          .pipe(concat(bundle.filename))
+          .pipe(sourcemaps.write('/'))
+          .pipe(gulp.dest('./public/js/' + bundle.destination));
+      }
+    )
+  );
+});
+
+gulp.task('img', function () {
+  gulp
+    .src(['./assets/images/**/*'])
+    .pipe(gulp.dest('./public/images'));
+});
+
+gulp.task('watch', ['img', 'sass', 'js'], function () {
   gulp.watch([
     './assets/sass/**/*.scss'
   ], ['sass']);
+  gulp.watch([
+    './gulp-bundles.js',
+    './assets/js/*.js',
+    './assets/js/**/*.js',
+  ], ['js']);
 });
 
-gulp.task('default', ['img', 'sass', 'watch']);
+gulp.task('default', ['img', 'sass', 'js', 'watch']);
