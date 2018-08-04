fa633978bf9e0665259edeccb172023342b99387
Andrey Rudenko <andrey.rudenko@gmail.com>
2018-08-05 01:02:43 +0300

    Add bundle for login screen.

---
+++ gulp-bundles.js
@@ -0,0 +1,15 @@
+var bundles = [
+  {
+    filename: 'login.min.js',
+    destination: './',
+    dependencies: [
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/bootstrap/dist/js/bootstrap.min.js'
+    ],
+    sources: [
+
+    ]
+  }
+];
+
+module.exports.bundles = bundles;
